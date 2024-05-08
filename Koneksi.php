410fb2cdba0061033dde4db7181a9b0505ebfa6f
raisyaramadhani <raisyachmad@gmail.com>
2024-05-08 10:30:29 +0700

    koneksi

---
+++ Koneksi.php
@@ -0,0 +1,3 @@
+<?php
+$conn=pg_connect("host=localhost port=5432 dbname=ERD PPDB user=postgres password=12345");
+?>
--- README.md
+++
@@ -1,3 +0,0 @@
-<?php
-$conn=pg_connect("host=localhost port=5432 dbname=ERD PPDB user=postgres password=12345");
-?>
